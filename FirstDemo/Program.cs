3f88d60a3f09ee77919c7136c28984ceb8c598ec
aniketkalvit <aniketkalvit@gmail.com>
2022-09-29 14:47:25 +0530

    Add project files.

---
+++ FirstDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 2022;
+            string month = "Sept";
+           
+
+            // interpolation --> $ sign
+
+            Console.WriteLine($"\nCurrent year is {year} \t & month is {month}");
+       
+        }
+    }
+}
